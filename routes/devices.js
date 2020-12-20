ffff0d66d958042f962b0065244bd13412ef066e
Ubuntu <ubuntu@ip-172-31-11-66.ap-south-1.compute.internal>
2020-12-20 17:39:48 +0000

    Route Error Recified

--- routes/devices.js
+++ routes/devices.js
@@ -8,6 +8,12 @@ router.get('/', (req,res) => {
         .catch((err) => res.status(400).json("Error: "+err));
 })
 
+router.get('/:id', (req,res) => {
+    Devices.findOne({ _id: req.params.id })
+	.then((device) => res.json(device))
+	.catch((err) => res.status(400).json("Error: "+err));
+})
+
 router.post('/add', (req,res) => {
     const newDevice = new Devices({
         dname: req.body.dname
@@ -19,7 +25,7 @@ router.post('/add', (req,res) => {
 
 router.post('/update/:id', (req,res) => {
     Devices.updateOne(
-      { id: req.params.id},
+      { _id: req.params.id},
       { dstatus: Number(req.body.status)}
     )
       .then(() => res.json("Device Status Updated"))
